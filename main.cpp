03001ca198f0bfa67099e41dd4c5172b4d947854
44aura <krivopisha45@gmail.com>
2024-05-13 23:30:11 +0300

    First commit

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <chrono>
+#include <thread>
+#include <iostream>
+
+volatile char glob_char = 0;
+int main(int argc, char* argv[]){
+    if(*argv[1] != '1' && *argv[1] != '2'){
+        std::cout << "Такого режима нет, попробуйте снова\n";
+        return 1;
+    }
+    constexpr size_t page_size = 4 * (1 << 10);
+    constexpr size_t arr_size = 20UL * (1 << 30);
+    char* arr = new char[arr_size];
+    int delay_ms = 10;
+
+    size_t i = 0;
+    while(i < arr_size){
+        if (*argv[1] == '1'){
+             glob_char = arr[i];
+        } 
+        else if(*argv[1] == '2'){
+            arr[i] = 0;
+        }
+        i += page_size;
+        if (i / page_size % 2000 == 0){
+            std::this_thread::sleep_for(std::chrono::milliseconds(delay_ms));
+        }
+    }
+
+    delete[] arr;
+    return 0;
+}
